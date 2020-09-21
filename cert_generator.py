99e176dbc2f80146d4f0b96c76d90dffb3f50fec
codermoderSD <67367732+codermoderSD@users.noreply.github.com>
2020-09-21 19:29:00 +0530

    Update cert_generator.py

--- cert_generator.py
+++ cert_generator.py
@@ -1,77 +1,73 @@
 # from PIL import Image, ImageFont, ImageDraw
 import pandas as pd
-# import numpy
+import numpy
 import smtplib 
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText 
 from email.mime.base import MIMEBase 
 from email import encoders 
 
-# def createCertificate(name):
-#     FONT_COLOR = "#000000"
-#     WIDTH, HEIGHT = 3627, 1253
-#     image_source = Image.open(r'dummy.jpg')
-#     draw = ImageDraw.Draw(image_source)
-#     font = ImageFont.truetype("arial.ttf", 150)
-#     w, h= draw.textsize(name, font=font)
-#     draw.text(((WIDTH-w)/2,1200), name, fill=FONT_COLOR, font=font)
-#     image_source.save("\shubham\Cert_&_emails_python\\" + "Participation_cert"+".jpg")
-#     print('printing certificate of: '+name)
-    
-def sendMail(name,email):
-    fromaddr = "sdisilva13@gmail.com"
-    #"*Add your email*"
+def sendMail(first_name, name,email):
+    fromaddr = "sdisilva13@gmail.com"      #change
     toaddr = email
     msg = MIMEMultipart()  
     msg['From'] = fromaddr 
     msg['To'] = toaddr  
     msg['Subject'] = "Certificate of Participation"
-    body = """\
+    body = f"""\
     <html>
+    <head>
+        <meta first_name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
     <body>
+        <h1>Greetings {first_name}</h1>
         <p><b>Thanks for attending workshop on Gesture Controlled Gaming using OpenCV</b></p>
-        <p><b>Kindly find your E-certificate for the same, attached along with this mail.<br>We look forward seeing you at all our future workshops, seminars and events!</b><p>
-        <p><b>Regards<br>IEEE-VIT Student Branch</b></p>
-        <div style="text-align:center">
-            <p><b>Connect with us on</b></p>
-            
-            <a href="https://www.facebook.com/IEEEVIT1"><img src="https://www.bworldonline.com/wp-content/uploads/2020/08/f_logo_RGB-Hex-Blue_512.png" height="45" width="45"></img></a>
-            
-            <a href="https://www.instagram.com/ieeevit/?hl=en"><img src="https://static01.nyt.com/images/2016/05/11/us/12xp-instagram/12xp-instagram-facebookJumbo-v2.jpg" height="55" width="100"></img></a>
-            
-            <a href="https://ieee.vit.edu.in/"><img src="https://ieee.vit.edu.in/assets/images/ieeevit-blue-1-5017x1103.png" height="55" width="100"></img></a>
-            <p><b>Ask us anything about programming, meet like minded people, build projects.</b></p>
-            <p><b>Join the coders Republic Group now:</b></p>
-            <a href="https://chat.whatsapp.com/GNjVY5fSZav73fl77vGPj2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/600px-WhatsApp_logo-color-vertical.svg.png" height="50" width="50"></img></a>
-            <p><b>For any error in certificate <a>click here</a></b></p>
-        </div>
-        <img src="cid:{image_cid}">
+        <br><br>Kindly find your E-Certificate for the same, attached along with this mail.
+        <br>We look forward to seeing you at all our future workshops, seminars and events!
+        <br><br>Regards,
+        <br>IEEE-VIT Student Branch
+        <br><center><br>Connect with us on
+        <br><p><a href="http://ieee.vit.edu.in/index.html"><img src="https://ieee.vit.edu.in/assets/images/ieeevit-blue-1-5017x1103.png" height="50" width="200" hspace="20"><a href="https://www.instagram.com/ieeevit/"><img src="https://images-na.ssl-images-amazon.com/images/I/71VQR1WetdL.png" height="50" width="50" hspace="20"><a href="https://www.facebook.com/IEEEVIT1/"><img src="https://ioufinancial.com/wp-content/uploads/2017/02/facebook.png" height="50" width="50" hspace="20"></a>
+        <br><br>
+        <font size="2">Ask us anything about programming, meet like minded people, build projects.<br>
+        <center>Join the Coders Republic Group now:</font><br><br>
+        <a href="https://chat.whatsapp.com/GNjVY5fSZav73fl77vGPj2"><img src="http://www.idroexpert.com/wp-content/uploads/soon-873316_960_720.png" height="50" width="50" hspace="20"></a><br><br>
+        <p>For any errors in certificate<a href="https://goo.gl/forms/8dqLFOmrG3KZs65f2"> click here</a></p>
     </body>
     </html>
-    """ 
-    msg.attach(MIMEText(body, 'html'))  
-    filename = name
-    #add your path
-    attachment = open(f'D:\Shubham\shubham\Cert_&_emails_python\workshop1\{filename}', "rb")
-    # attachment1 = open(f"D:\Shubham\shubham\Python_udemy\workshop\Workshop_Codes", "rb")
+    """
+
+    # for certificate
+    certificate = open(f"D:/Shubham/shubham/Cert_&_emails_python/workshop1/{name}.png", "rb")  #change
+    msg.attach(MIMEText(body, 'html'))
+    filename = f"{name}.png"
     p = MIMEBase('application', 'octet-stream') 
-    p.set_payload((attachment).read())
-    # p.set_payload((attachment1).read())
+    p.set_payload((certificate).read())
     encoders.encode_base64(p) 
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    # p.add_header('Content-Disposition', "attachment1; filename= %s" % filename)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
     msg.attach(p)
-    s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+    #for zipfile
+    zipfile = open("D:/Shubham/shubham/Cert_&_emails_python/OpenCV_workshop.zip", "rb")
+    filename1 = "OpenCV_workshop.zip"
+    p1 = MIMEBase('application', 'octet-stream')
+    p1.set_payload((zipfile).read())
+    encoders.encode_base64(p1)
+    p1.add_header('Content-Disposition', "attachment; filename= %s" % filename1)
+    msg.attach(p1)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls() 
-    s.login(fromaddr, "saviosavio") 
-# *Your Email password*
+    s.login(fromaddr, "saviosavio")                                      #change
     text = msg.as_string() 
     s.sendmail(fromaddr, toaddr, text) 
     s.quit() 
 
-df = pd.read_csv("cert1.csv")
+
+
+df = pd.read_csv("D:/Shubham/shubham/Cert_&_emails_python/cert1.csv")   #Change
 for i in range(len(df)):
     name = df.iloc[i,0]
     email = df.iloc[i,1]
-    # createCertificate(name)
-    sendMail(name, email)
+    first_name = name.split()[0]
+    sendMail(first_name ,name, email)
